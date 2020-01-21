6f1c1e96a64560df46f49209b66a1f83cfcf8154
Iryna Tkachenka <Iryna_Tkachenka@epam.com>
2020-01-21 17:45:32 +0300

    fixed comments

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
+// function onResize(x) {
+//     let headerElement = window.document.getElementsByClassName("hero-block__header")[0];
+//     console.log(headerElement);
+//     if (x.matches) {
+//         headerElement.className = "hero-block__header display-none"
+//     } else {
+//         headerElement.className = "hero-block__header"
+//     }
+// }
+
+// var x = window.matchMedia('(max-width: 1130px)')
+// onResize(x);
+// x.addListener(onResize);
+
 function onClick() {
     let headerElement = window.document.getElementsByClassName("hero-block__header")[0];
-    headerElement.style.display === "block"
-    ? headerElement.style.display = "none"
-    : headerElement.style.display = "block";
+    headerElement.className === "hero-block__header display-none"
+    ? headerElement.className = "hero-block__header display-block"
+    : headerElement.className = "hero-block__header display-none"
 }
